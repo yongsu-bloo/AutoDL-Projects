570ca2f8bb818dec8dfbacaf5b900e0072275185
yongsu-bloo <yongsubaek7@gmail.com>
2020-01-20 20:21:38 +0900

    fitnet-train-v2 add

--- exps/fitnet-train-v2.py
+++ exps/fitnet-train-v2.py
@@ -38,8 +38,6 @@ def main(args):
   optim_config = load_config(args.optim_config,
                                 {'class_num': class_num, 'KD_alpha': args.KD_alpha, 'KD_temperature': args.KD_temperature},
                                 logger)
-  matching_optim_config = load_config(args.matching_optim_config, {'class_num': class_num}, logger)
-
   #Student
   if args.student_config is None:
       search_space = get_search_spaces('cell', args.search_space_name)
@@ -56,6 +54,7 @@ def main(args):
 
       student_model = get_cell_based_tiny_net(student_config)
   else:
+      ## Not Tested ##
       student_config = load_config(args.student_config)
       if "cifar" in args.dataset:
           student_model = get_cifar_models(student_config)
@@ -74,7 +73,6 @@ def main(args):
   flop, param  = get_model_infos(student_model, xshape)
   student_weights = student_model.get_weights() if args.student_config is None else list(student_model.parameters())
   w_optimizer, w_scheduler, criterion = get_optim_scheduler(student_weights, optim_config)
-  h_optimizer, h_scheduler, _ = get_optim_scheduler(student_weights + list(matching_layers.parameters()), matching_optim_config)
 
   criterion = criterion.cuda()
   # logger.log('Teacher ====>>>>:\n{:}'.format(teacher_base))
@@ -88,8 +86,6 @@ def main(args):
   logger.log('w-optimizer : {:}'.format(w_optimizer))
   logger.log('w-scheduler : {:}'.format(w_scheduler))
   logger.log('criterion   : {:}'.format(criterion))
-  logger.log('h-optimizer : {:}'.format(h_optimizer))
-  logger.log('h-scheduler : {:}'.format(h_scheduler))
 
   last_info, model_base_path, model_best_path = logger.path('info'), logger.path('model'), logger.path('best')
   if last_info.exists() and not args.overwrite: # automatically resume from previous checkpoint
@@ -105,13 +101,9 @@ def main(args):
     matching_layers.load_state_dict( checkpoint['matching_layers'] )
     w_scheduler.load_state_dict ( checkpoint['w_scheduler'] )
     w_optimizer.load_state_dict ( checkpoint['w_optimizer'] )
-    h_scheduler.load_state_dict ( checkpoint['h_scheduler'] )
-    h_optimizer.load_state_dict ( checkpoint['h_optimizer'] )
     max_bytes        = checkpoint['max_bytes']
-    hint_results = checkpoint['hint_results']
     train_results = checkpoint['train_results']
     valid_results = checkpoint['valid_results']
-    (train_hint_losses, valid_hint_losses) = hint_results
     (train_losses, train_acc1s, train_acc5s) = train_results
     (valid_losses, valid_acc1s, valid_acc5s) = valid_results
     logger.log("=> loading checkpoint of the last-info '{:}' start with {:}-th epoch.".format(last_info, start_epoch))
@@ -127,13 +119,9 @@ def main(args):
     matching_layers.load_state_dict( checkpoint['matching_layers'] )
     w_scheduler.load_state_dict ( checkpoint['w_scheduler'] )
     w_optimizer.load_state_dict ( checkpoint['w_optimizer'] )
-    h_scheduler.load_state_dict ( checkpoint['h_scheduler'] )
-    h_optimizer.load_state_dict ( checkpoint['h_optimizer'] )
     max_bytes        = checkpoint['max_bytes']
-    hint_results = checkpoint['hint_results']
     train_results = checkpoint['train_results']
     valid_results = checkpoint['valid_results']
-    (train_hint_losses, valid_hint_losses) = hint_results
     (train_losses, train_acc1s, train_acc5s) = train_results
     (valid_losses, valid_acc1s, valid_acc5s) = valid_results
     logger.log("=> loading checkpoint from '{:}' start with {:}-th epoch.".format(args.resume, start_epoch))
@@ -147,8 +135,6 @@ def main(args):
         (arch_train_result, arch_test_result) = nor_train_results
     matching_layers.load_state_dict( checkpoint['matching_layers'] )
     start_epoch = 0
-    train_hint_losses = []
-    valid_hint_losses = {'best': float('inf')}
     train_losses = []
     train_acc1s = []
     train_acc5s = []
@@ -161,7 +147,10 @@ def main(args):
     # genotype sampling
     api = None
     if args.student_config is None:
-        if args.arch_str is None:
+        if args.arch_str:
+            genotype = Structure.str2structure(args.arch_str)
+        else:
+            # sample an architecture
             if args.sample_method == 'best':
                 genotype = search_model.genotype() # get the best arch
             elif args.sample_method == 'infer':
@@ -170,9 +159,6 @@ def main(args):
                 genotype = student_model.dync_genotype(use_random=True) # sample uniformly
             else:
                 raise ValueError('invalid sample_method: {:}'.format(args.sample_method))
-        else:
-            genotype = Structure.str2structure(args.arch_str)
-
         network.module.set_cal_mode('dynamic', genotype)
 
         if args.search_space_name == "nas-bench-201":
@@ -189,8 +175,6 @@ def main(args):
             logger.log("\nNormal Training Result-12epoch:\n{:}\n".format(api.query_by_arch(genotype, True)))
             logger.log("\nNormal Training Result-200epoch:\n{:}\n".format(api.query_by_arch(genotype)))
     start_epoch, max_bytes = 0, {}
-    train_hint_losses = []
-    valid_hint_losses = {'best': float('inf')}
     train_losses = []
     train_acc1s = []
     train_acc5s = []
@@ -198,51 +182,9 @@ def main(args):
     valid_acc1s = {'best': -1}
     valid_acc5s = []
 
-
   """Main Training and Evaluation Loop"""
-  if not args.resume and not last_info.exists() or args.overwrite:
-      # Hint Training First
-      start_time, hint_time, epoch_time, total_epoch = time.time(), AverageMeter(), AverageMeter(), matching_optim_config.epochs + matching_optim_config.warmup
-      if args.exp_name == "tmp": total_epoch = 2
-      fitnet_train_func, fitnet_valid_func = get_procedures("fitnet")
-      for epoch in range(start_epoch, total_epoch):
-        h_scheduler.update(epoch, 0.0)
-        need_time = 'Time Left: {:}'.format( convert_secs2time(epoch_time.avg * (total_epoch-epoch), True) )
-        epoch_str = 'epoch={:03d}/{:03d}'.format(epoch, total_epoch)
-        LRs       = h_scheduler.get_lr()
-        find_best = False
-
-        logger.log('\n***{:s}*** start {:s} {:s}, LR=[{:.6f} ~ {:.6f}], h_scheduler={:}'.format(time_string(), epoch_str, need_time, min(LRs), max(LRs), h_scheduler))
-
-        # train for one epoch
-        train_loss, _, _ = fitnet_train_func(train_loader, teacher, network, matching_layers, criterion, h_scheduler, h_optimizer, matching_optim_config, epoch_str, args.print_freq, logger)
-        hint_time.update(time.time() - start_time)
-        train_hint_losses.append(train_loss)
-        # log the results
-        logger.log('***{:s}*** HINT TRAIN [{:}] loss = {:.6f} time-cost = {:.1f}'.format(time_string(), epoch_str, train_loss, hint_time.sum))
-        # measure elapsed time
-        epoch_time.update(time.time() - start_time)
-
-        # evaluate the performance
-        if (epoch % args.eval_frequency == 0) or (epoch + 1 == total_epoch):
-          logger.log('-'*150)
-          valid_loss, _, _ = fitnet_valid_func(valid_loader, teacher, network, matching_layers, criterion, optim_config, epoch_str, args.print_freq_eval, logger)
-          valid_hint_losses[epoch] = valid_loss
-          logger.log('***{:s}*** VALID [{:}] loss = {:.6f} | Best-Loss={:.6f}'.format(time_string(), epoch_str, valid_loss, valid_hint_losses['best']))
-          if valid_loss <= valid_hint_losses['best']:
-            valid_hint_losses['best'] = valid_loss
-            find_best                = True
-            logger.log('Currently, the best validation hint loss found at {:03d}-epoch :: loss={:.6f}'.format(epoch, valid_loss))
-          num_bytes = torch.cuda.max_memory_cached( next(network.parameters()).device ) * 1.0
-          logger.log('[GPU-Memory-Usage on {:} is {:} bytes, {:.2f} KB, {:.2f} MB, {:.2f} GB.]'.format(next(network.parameters()).device, int(num_bytes), num_bytes / 1e3, num_bytes / 1e6, num_bytes / 1e9))
-          max_bytes[epoch] = num_bytes
-        if epoch % 10 == 0: torch.cuda.empty_cache()
-        start_time = time.time()
-
-  # Guided layer training
   start_time, train_time, epoch_time, total_epoch = time.time(), AverageMeter(), AverageMeter(), optim_config.epochs + optim_config.warmup
-  if args.exp_name == "tmp": total_epoch = 2
-  train_func, valid_func = get_procedures(args.procedure)
+  train_func, valid_func = get_procedures("fitnet")
   for epoch in range(start_epoch, total_epoch):
     w_scheduler.update(epoch, 0.0)
     need_time = 'Time Left: {:}'.format( convert_secs2time(epoch_time.avg * (total_epoch-epoch), True) )
@@ -253,13 +195,16 @@ def main(args):
     logger.log('\n***{:s}*** start {:s} {:s}, LR=[{:.6f} ~ {:.6f}], w_scheduler={:}'.format(time_string(), epoch_str, need_time, min(LRs), max(LRs), w_scheduler))
 
     # train for one epoch
+    if args.bottom_beta is not None:
+        matching_layers.beta = (args.beta-args.bottom_beta)*(1. - (epoch / total_epoch)) + args.bottom_beta
     if args.procedure == "Simple-KD":
-        kd_coef = 4. - 3.*(epoch / total_epoch)
-        train_loss, train_acc1, train_acc5 = train_func(train_loader, teacher, network, criterion, w_scheduler, w_optimizer, optim_config, epoch_str, args.print_freq, logger, kd_coef)
-    elif args.procedure == "basic":
-        train_loss, train_acc1, train_acc5 = train_func(train_loader, network, criterion, w_scheduler, w_optimizer, optim_config, epoch_str, args.print_freq, logger)
+        kd_coef = 1.
+    elif args.procedure == "Moving-KD":
+        kd_coef = 1. - 0.75*(epoch / total_epoch)
     else:
-        raise ValueError("invalid procedue name: {:}".format(args.procedure))
+        kd_coef = 0.
+    train_loss, train_acc1, train_acc5, train_matching_loss, train_kd_loss \
+        = train_func(train_loader, teacher, network, matching_layers, criterion, w_scheduler, w_optimizer, optim_config, epoch_str, args.print_freq, logger, kd_coef=kd_coef, version=2)
     train_time.update(time.time() - start_time)
     # log the results
     train_losses.append(train_loss); train_acc1s.append(train_acc1); train_acc5s.append(train_acc5)
@@ -268,12 +213,8 @@ def main(args):
     # evaluate the performance
     if (epoch % args.eval_frequency == 0) or (epoch + 1 == total_epoch):
       logger.log('-'*150)
-      if args.procedure == "Simple-KD":
-          valid_loss, valid_acc1, valid_acc5 = valid_func(valid_loader, teacher, network, criterion, optim_config, epoch_str, args.print_freq_eval, logger)
-      elif args.procedure == "basic":
-          valid_loss, valid_acc1, valid_acc5 = valid_func(valid_loader, network, criterion, optim_config, epoch_str, args.print_freq_eval, logger)
-      else:
-          raise ValueError("invalid procedue name: {:}".format(args.procedure))
+      valid_loss, valid_acc1, valid_acc5, valid_matching_loss, valid_kd_loss \
+        = valid_func(valid_loader, teacher, network, matching_layers, criterion, optim_config, epoch_str, args.print_freq_eval, logger, kd_coef=kd_coef, version=2)
       valid_acc1s[epoch] = valid_acc1
       valid_losses.append(valid_loss); valid_acc5s.append(valid_acc5)
       logger.log('***{:s}*** VALID [{:}] loss = {:.6f}, accuracy@1 = {:.2f}, accuracy@5 = {:.2f} | Best-Valid-Acc@1={:.2f}, Error@1={:.2f}'.format(time_string(), epoch_str, valid_loss, valid_acc1, valid_acc5, valid_acc1s['best'], 100-valid_acc1s['best']))
@@ -287,7 +228,6 @@ def main(args):
     if epoch % 10 == 0: torch.cuda.empty_cache()
 
     # save checkpoint
-    hint_results = (train_hint_losses, valid_hint_losses)
     train_results = (train_losses, train_acc1s, train_acc5s)
     valid_results = (valid_losses, valid_acc1s, valid_acc5s)
     save_path = save_checkpoint({
@@ -300,14 +240,10 @@ def main(args):
           'genotype'     : genotype.tostr() if args.student_config is None else "",
           'nor_train_results'   : deepcopy(nor_train_results) if args.student_config is None and args.search_space_name == "nas-bench-201" else {},
           'optim_config' : optim_config._asdict(),
-          'matching_optim_config' : matching_optim_config._asdict(),
           'student_model'   : student_model.state_dict(),
           'matching_layers' : matching_layers.state_dict(),
           'w_scheduler'    : w_scheduler.state_dict(),
           'w_optimizer'    : w_optimizer.state_dict(),
-          'h_scheduler'    : h_scheduler.state_dict(),
-          'h_optimizer'    : h_optimizer.state_dict(),
-          'hint_results'    : deepcopy(hint_results),
           'train_results'   : deepcopy(train_results),
           'valid_results'   : deepcopy(valid_results),
           }, model_base_path, logger)
@@ -342,7 +278,7 @@ if __name__ == '__main__':
   parser.add_argument('--resume'      ,     type=str,                   help='Resume path.')
   parser.add_argument('--init_model'  ,     type=str,                   help='The initialization model path.')
   # Save and Load
-  parser.add_argument('--save_dir',           type=str,   default="./output/transfer-train",     help='Folder to save checkpoints and log.')
+  parser.add_argument('--save_dir',           type=str,   default="./output/transfer-train-v2",     help='Folder to save checkpoints and log.')
   parser.add_argument('--dataset',            type=str,   default='cifar10', choices=['cifar10', 'cifar100', 'ImageNet16-120'], help='Choose between Cifar10/100 and ImageNet-16.')
   parser.add_argument('--data_path',          type=str,   default=os.environ['TORCH_HOME'] + "/cifar.python", help='Path to dataset')
   parser.add_argument('--cutout_length',    type=int,     default=-1,      help='The cutout length, negative means not use.')
@@ -352,11 +288,15 @@ if __name__ == '__main__':
   parser.add_argument('--num_cells',          type=int,   default=5, help='The number of cells in one stage.')
 
   parser.add_argument('--beta', type=float, default=2e-2, help='matching loss scale')
+  parser.add_argument('--bottom_beta', type=float, help='moving matching loss scale destination')
   # NAS settings
   parser.add_argument('--search_space_name',  type=str,   default="nas-bench-201", help='The search space name.')
   parser.add_argument('--sample_method',    type=str,     default='uniform', choices=['uniform', 'infer', 'best'],    help='The architecture sampling method: uniform, infer, best')
   parser.add_argument('--select_num',         type=int,   default=100, help='The number of selected architectures to evaluate.')
   parser.add_argument('--arch_str',         type=str,       default=None, help="specific architecture to be transfer trained")
+  # architecture leraning rate
+  # parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for arch encoding')
+  # parser.add_argument('--arch_weight_decay',  type=float, default=1e-3, help='weight decay for arch encoding')
   # parser.add_argument('--arch_nas_dataset',   type=str,   help='The path to load the architecture dataset (tiny-nas-benchmark).')
   # Training Options
   parser.add_argument('--procedure'   ,     type=str,        default='basic',       help='The procedure basic prefix.')
@@ -368,9 +308,8 @@ if __name__ == '__main__':
   parser.add_argument('--print_freq_eval',  type=int,   default=100,    help='print frequency (default: 100)')
   parser.add_argument('--track_running_stats',type=int,   default=1, choices=[0,1],help='Whether use track_running_stats or not in the BN layer.')
   # Configs and Checkpoints
-  parser.add_argument('--student_config',     type=str,    default=None,      help='The path to the student model configuration')
-  parser.add_argument('--optim_config',     type=str,    default="./configs/opts/CIFAR-fitnet-nas102.config",      help='The path to the optimizer configuration')
-  parser.add_argument('--matching_optim_config',     type=str,  default="./configs/opts/CIFAR-fitnet-nas102-hint.config",        help='The path to the hint optimizer configuration')
+  parser.add_argument('--student_config',     type=str,    help='The path to the student model configuration')
+  parser.add_argument('--optim_config',     type=str,    default="./configs/opts/CIFAR-fitnet-nas102-hint.config",      help='The path to the optimizer configuration')
   parser.add_argument('--student_checkpoint',    type=str,    default="./output/search-cell-nas-bench-102/SETN-cifar10/checkpoint/seed-36878-basic.pth",          help='The student checkpoint in knowledge distillation.')
   parser.add_argument('--teacher_checkpoint',    type=str,    default="./.latent-data/basemodels/cifar10/ResNet56.pth",          help='The teacher checkpoint in knowledge distillation.')
   # Acceleration
--- lib/procedures/fitnet_main.py
+++ lib/procedures/fitnet_main.py
@@ -8,32 +8,38 @@ from log_utils import AverageMeter, time_string
 from utils     import obtain_accuracy
 
 
-def fitnet_train(xloader, teacher, network, matching_layers, criterion, scheduler, optimizer, optim_config, extra_info, print_freq, logger):
-  loss, acc1, acc5 = procedure(xloader, teacher, network, matching_layers, criterion, scheduler, optimizer, 'train', optim_config, extra_info, print_freq, logger)
-  return loss, acc1, acc5
-
-def fitnet_valid(xloader, teacher, network, matching_layers, criterion, optim_config, extra_info, print_freq, logger):
+def fitnet_train(xloader, teacher, network, matching_layers, criterion, scheduler, optimizer, optim_config, extra_info, print_freq, logger, kd_coef=0., version=1):
+    return procedure(xloader, teacher, network, matching_layers, criterion, scheduler, optimizer, 'train', optim_config, extra_info, print_freq, logger, \
+                                 kd_coef, version)
+    return
+def fitnet_valid(xloader, teacher, network, matching_layers, criterion, optim_config, extra_info, print_freq, logger, kd_coef=0., version=1):
   with torch.no_grad():
-    loss, acc1, acc5 = procedure(xloader, teacher, network, matching_layers, criterion, None, None, 'valid', optim_config, extra_info, print_freq, logger)
-  return loss, acc1, acc5
+    return procedure(xloader, teacher, network, matching_layers, criterion, None, None, 'valid', optim_config, extra_info, print_freq, logger, \
+                                 kd_coef, version)
 
 
-def loss_KD_fn(criterion, student_logits, teacher_logits, studentFeatures, teacherFeatures, targets, alpha, temperature):
+def loss_KD_fn(criterion, student_logits, teacher_logits, studentFeatures, teacherFeatures, targets, alpha, temperature, kd_coef=0.):
   # Normal + KD training
   basic_loss = criterion(student_logits, targets) * (1. - alpha)
-  log_student= F.log_softmax(student_logits / temperature, dim=1)
-  sof_teacher= F.softmax    (teacher_logits / temperature, dim=1)
-  KD_loss    = F.kl_div(log_student, sof_teacher, reduction='batchmean') * (alpha * temperature * temperature)
-  return basic_loss + KD_loss
+  if kd_coef != 0.:
+      log_student= F.log_softmax(student_logits / temperature, dim=1)
+      sof_teacher= F.softmax    (teacher_logits / temperature, dim=1)
+      KD_loss    = F.kl_div(log_student, sof_teacher, reduction='batchmean') * (alpha * temperature * temperature)
+  else:
+      KD_loss = 0.
+  return basic_loss + kd_coef * KD_loss
 
 def loss_fitnet_fn(teacher_features, student_features, matching_layers):
     # matching layer + guided training
     matching_loss = matching_layers(teacher_features, student_features)
     return torch.mean(matching_loss)
 
-def procedure(xloader, teacher, network, matching_layers, criterion, scheduler, optimizer, mode, config, extra_info, print_freq, logger):
+def procedure(xloader, teacher, network, matching_layers, criterion, scheduler, optimizer, mode, config, extra_info, print_freq, logger, \
+              kd_coef=0., version=1):
   data_time, batch_time, losses, top1, top5 = AverageMeter(), AverageMeter(), AverageMeter(), AverageMeter(), AverageMeter()
   Ttop1, Ttop5 = AverageMeter(), AverageMeter()
+  if version == 2:
+      matching_losses, kd_losses = AverageMeter(), AverageMeter()
   if mode == 'train':
     network.train()
     matching_layers.train()
@@ -43,10 +49,6 @@ def procedure(xloader, teacher, network, matching_layers, criterion, scheduler, 
   else: raise ValueError("The mode is not right : {:}".format(mode))
   teacher.eval()
   # update the weights
-  # sampled_arch = network.module.dync_genotype(True) # uniform sampling
-  # network.module.set_cal_mode('dynamic', sampled_arch)
-  # print("\n\nNew Architecture Sampled\n: {:}\n\n".format(sampled_arch))
-  # logger.log('[{:5s}] config :: auxiliary={:}, KD :: [alpha={:.2f}, temperature={:.2f}]'.format(mode, config.auxiliary if hasattr(config, 'auxiliary') else -1, config.KD_alpha, config.KD_temperature))
   end = time.time()
   for i, (inputs, targets) in enumerate(xloader):
     if mode == 'train': scheduler.update(None, 1.0 * i / len(xloader))
@@ -57,20 +59,23 @@ def procedure(xloader, teacher, network, matching_layers, criterion, scheduler, 
 
     if mode == 'train': optimizer.zero_grad()
 
-    _, logits, student_features = network(inputs, out_all=True)
+    student_f, logits, student_features = network(inputs, out_all=True)
     if isinstance(logits, list):
       assert len(logits) == 2, 'logits must has {:} items instead of {:}'.format(2, len(logits))
       logits, logits_aux = logits
     else:
       logits, logits_aux = logits, None
     with torch.no_grad():
-      _, teacher_logits, teacher_features = teacher(inputs, out_all=True)
-
-    # loss             = loss_KD_fn(criterion, logits, teacher_logits, student_f, teacher_f, targets, config.KD_alpha, config.KD_temperature)
+      teacher_f, teacher_logits, teacher_features = teacher(inputs, out_all=True)
     loss = loss_fitnet_fn(teacher_features, student_features, matching_layers)
-    # if config is not None and logits_aux is not None and hasattr(config, 'auxiliary') and config.auxiliary > 0:
-    #   loss_aux = criterion(logits_aux, targets)
-    #   loss += config.auxiliary * loss_aux
+    if version == 2:
+      matching_losses.update(loss.item(), inputs.size(0))
+      loss_KD = loss_KD_fn(criterion, logits, teacher_logits, student_f, teacher_f, targets, config.KD_alpha, config.KD_temperature, kd_coef)
+      kd_losses.update(loss_KD.item(), inputs.size(0))
+      loss += loss_KD
+    if config is not None and logits_aux is not None and hasattr(config, 'auxiliary') and config.auxiliary > 0:
+      loss_aux = criterion(logits_aux, targets)
+      loss += config.auxiliary * loss_aux
 
     if mode == 'train':
       loss.backward()
@@ -96,10 +101,14 @@ def procedure(xloader, teacher, network, matching_layers, criterion, scheduler, 
         Sstr += ' {:}'.format(scheduler.get_min_info())
       Tstr = 'Time {batch_time.val:.2f} ({batch_time.avg:.2f}) Data {data_time.val:.2f} ({data_time.avg:.2f})'.format(batch_time=batch_time, data_time=data_time)
       Lstr = 'Loss {loss.val:.3f} ({loss.avg:.3f})  Prec@1 {top1.val:.2f} ({top1.avg:.2f}) Prec@5 {top5.val:.2f} ({top5.avg:.2f})'.format(loss=losses, top1=top1, top5=top5)
+      if version == 2:
+          Lstr += ' Matching Loss {matching_loss.val:.3f} ({matching_loss.avg:.3f}) KD+Cls Loss {kd_loss.val:.3f} ({kd_loss.avg:.3f})'.format(matching_loss=matching_losses, kd_loss=kd_losses)
       Lstr+= ' Teacher : acc@1={:.2f}, acc@5={:.2f}'.format(Ttop1.avg, Ttop5.avg)
       Istr = 'Size={:}'.format(list(inputs.size()))
       logger.log(Sstr + ' ' + Tstr + ' ' + Lstr + ' ' + Istr)
 
-  # logger.log(' **HINT {:5s}** accuracy drop :: @1={:.2f}, @5={:.2f}'.format(mode.upper(), Ttop1.avg - top1.avg, Ttop5.avg - top5.avg))
-  # logger.log(' **{mode:5s}** Prec@1 {top1.avg:.2f} Prec@5 {top5.avg:.2f} Error@1 {error1:.2f} Error@5 {error5:.2f} Loss:{loss:.3f}'.format(mode=mode.upper(), top1=top1, top5=top5, error1=100-top1.avg, error5=100-top5.avg, loss=losses.avg))
+  if version == 2:
+      logger.log(' **HINT {:5s}** accuracy drop :: @1={:.2f}, @5={:.2f}'.format(mode.upper(), Ttop1.avg - top1.avg, Ttop5.avg - top5.avg))
+      logger.log(' **{mode:5s}** Prec@1 {top1.avg:.2f} Prec@5 {top5.avg:.2f} Error@1 {error1:.2f} Error@5 {error5:.2f} Loss:{loss:.3f}'.format(mode=mode.upper(), top1=top1, top5=top5, error1=100-top1.avg, error5=100-top5.avg, loss=losses.avg))
+      return losses.avg, top1.avg, top5.avg, matching_losses.avg, kd_losses.avg
   return losses.avg, top1.avg, top5.avg
